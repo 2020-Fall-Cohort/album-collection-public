07d37a8970a5388dccdf72a5fe92fb5f0c45133f
Maureen Joyce <mjoyce10@kent.edu>
2020-11-12 13:34:54 -0500

    added components folder. added header and home files. added home and header functions to main

---
+++ frontend/src/js/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return `
+    <nav class='nav-header'>
+        <a class='nav-home'>Home</a>
+        <a>Artists</a>
+        <a>Albums</a>
+    </nav>
+    `
+}
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,7 +1,21 @@
-function header(){
-    const headerElement = document.querySelector('.header');
-    headerElement.innerText = ("IT WORKS");
-}
+import Header from './components/Header';
+import Home from './components/Home';
+
 export default () => {
     header();
+    navHome();
+}
+
+const appDiv = document.querySelector('.app');
+
+function header(){
+    const headerElement = document.querySelector('.header');
+    headerElement.innerHTML = Header();
+}
+
+function navHome(){
+    const homeButton = document.querySelector('.nav-home');
+    homeButton.addEventListener('click', function(){
+        appDiv.innerHTML = Home();
+    })
 }
