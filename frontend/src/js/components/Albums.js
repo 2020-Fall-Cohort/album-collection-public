b7a94711ed5be888b6b89a331765e915b8373e9a
jihaddupuy <jihad.dupuy@gmail.com>
2020-11-18 15:03:58 -0500

    css styling added

--- frontend/src/js/components/Albums.js
+++ frontend/src/js/components/Albums.js
@@ -1,15 +1,19 @@
 export default function Albums(albums){
     return `
+  
     <h1>List of Albums</h1>
+    <div class="albums">
         ${albums.map(album => {
             return `
-            <hr />
-            <img src="../../../images/${album.image}" />
-            <p><strong>${album.name}</strong> by ${album.artist.name}</p>
-            <p><strong>Description:</strong> ${album.description}</p>
-            <p><strong>Rating:</strong> ${album.rating}</p>
-            <p><strong>Popular Track:</strong> ${album.popularTrack}</p>
+           <div class= "album">
+                <img src="../../../images/${album.image}" />
+                <p><strong>${album.name}</strong> by ${album.artist.name}</p>
+                <p><strong>Description:</strong> ${album.description}</p>
+                <p><strong>Rating:</strong> ${album.rating}</p>
+                <p><strong>Popular Track:</strong> ${album.popularTrack}</p>
+            </div>
             `
         }).join("")}
+    </div>
     `
 }
